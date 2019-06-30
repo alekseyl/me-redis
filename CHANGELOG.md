4232c27fbea568314b79c35da93ddd71cb1f6d01
alekseyl <leshchuk@gmail.com>
2019-06-30 13:03:09 +0300

    - 0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
+# 0.1.3
+* minor messaging changes for AWS config, warning popups not on config calls 
+but on prepend. Config calls just displays resulting config now
+
 # 0.1.2
 * add AwsConfigBlocker extension. Prepending MeRedis successor with this module 
 prevents error while using the MeRedis with AWS ElasticCache (AWS blocks all config calls).
 
 * added docker-compose for local testing comfort 
 
+* awesome_print prod dependency 
+ 
 # 0.1.1
 * add MockRedis extension 
--- lib/me_redis/aws_config_blocker.rb
+++ lib/me_redis/aws_config_blocker.rb
@@ -5,26 +5,23 @@ module MeRedis
   module AwsConfigBlocker
 
      def config( *args, &block )
-      print <<AWS_MSG
-     
-      \e[0;33;31;1m!!!!!!!!! MeRedis AWS CONFIG BLOCKER WARNING!!!!!! \e[0;33;31;0m
-      You introduced AwsConfigBlocker into the ancestors chain, that means that you intend to skip Redis config call,
-      because AWS does not support config get/set calls by throwing an exception. 
-      AwsConfigBlocker will block config call from reaching your Redis server!
+       print 'config was called, resulting me_config:'
+       ap self.class.me_config
+       {}
+     end
 
-      You are calling config with arguments:
+     def self.prepended(base)
+       print <<AWS_MSG
+        
+        \e[0;33;31;1m!!!!!!!!! MeRedis AWS CONFIG BLOCKER WARNING!!!!!! \e[0;33;31;0m
+        You introduced AwsConfigBlocker into the ancestors chain, that means that you intend to skip Redis config call,
+        because AWS does not support config get/set calls by throwing an exception. 
+        AwsConfigBlocker will block config call from reaching your Redis server!
+        
+        Don't forget to setup redis param group through the AWS UI with exact values, or you might face unexpected 
+        optimization degradation. For more details look into the README section.
 
 AWS_MSG
-
-       ap args
-
-       print <<END_MSG
-       
-
-       Don't forget to setup redis param group with exact values, or you might face unexpected optimization degradation. 
-       You can find more details in the README section.
-END_MSG
-       {}
-    end
-  end
+     end
+   end
 end
